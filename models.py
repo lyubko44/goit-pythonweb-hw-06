33a0f30efeab5b98a59ceb7f9f93bba93451dbfd
Liubomyr Skorokhod <lyubko44@gmail.com>
2025-03-27 21:53:22 -0700

    first step - creating models of db

---
+++ models.py
@@ -0,0 +1,63 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+
+Base = declarative_base()
+
+
+class Student(Base):
+    __tablename__ = "students"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    group_id = Column(Integer, ForeignKey("groups.id"))
+
+    group = relationship("Group", back_populates="students")
+    grades = relationship("Grade", back_populates="student")
+
+
+class Group(Base):
+    __tablename__ = "groups"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+    students = relationship("Student", back_populates="group")
+
+
+class Teacher(Base):
+    __tablename__ = "teachers"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+    subjects = relationship("Subject", back_populates="teacher")
+
+
+class Subject(Base):
+    __tablename__ = "subjects"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    teacher_id = Column(Integer, ForeignKey("teachers.id"))
+
+    teacher = relationship("Teacher", back_populates="subjects")
+    grades = relationship("Grade", back_populates="subject")
+
+
+class Grade(Base):
+    __tablename__ = "grades"
+    id = Column(Integer, primary_key=True)
+    student_id = Column(Integer, ForeignKey("students.id"))
+    subject_id = Column(Integer, ForeignKey("subjects.id"))
+    grade = Column(Integer, nullable=False)
+    date_received = Column(Date, nullable=False)
+
+    student = relationship("Student", back_populates="grades")
+    subject = relationship("Subject", back_populates="grades")
+
+
+# Database connection
+DATABASE_URL = "postgresql://postgres:mysecretpassword@localhost:5432/postgres"
+engine = create_engine(DATABASE_URL)
+Base.metadata.create_all(engine)
+
+# Create a new session
+Session = sessionmaker(bind=engine)
+session = Session()
